77af1cb5fab791f569afcdcab241f7f7288ca162
Dev Milton Ahemd <miltonahmed08@yahoo.com>
2025-05-25 00:54:25 +0400

    Apply Condition
    If values are "   ", show massage.
    And if values are Text, Then Show a message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 
 let PlayerOneInputFiled = document.querySelector("#playerOneInput")
 let playerOneButton = document.querySelector(".playerOneBtn")
+let displayedError = document.querySelector(".error")
 
 playerOneButton.addEventListener("click", function(){
+console.log(PlayerOneInputFiled.value)
+
+if(PlayerOneInputFiled.value == ""){
+  displayedError.style.display = "inline-block"
+}
+else{
+  if(Number(PlayerOneInputFiled.value)){
+    displayedError.innerHTML = ""
+
+     
+     
+  }
+  else{
+    displayedError.style.display = "inline-block"
+     displayedError.innerHTML = "Please Give a Number LIKE(1,2,3)"
+
+  }
+ 
+}
+
 
 })
