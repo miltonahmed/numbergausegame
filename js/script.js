044f9164578a27f3419b803cc03f3c83ffa45c39
Dev Milton Ahemd <miltonahmed08@yahoo.com>
2025-05-25 01:53:23 +0400

    Function
    Make Function Dynamic

--- js/script.js
+++ js/script.js
@@ -1,37 +1,50 @@
+let PlayerOneInputFiled = document.querySelector('.playerOneInput');
+let playerOneButton = document.querySelector('.playerOneBtn');
+let PlayerTwoInputFiled = document.querySelector('.playerTwoInput');
+let playerTwoButton = document.querySelector('.playerTwoBtn');
+let displayedError = document.querySelector('.error');
+let heading = document.querySelector('.heading');
 
-let PlayerOneInputFiled = document.querySelector(".playerOneInput")
-let playerOneButton = document.querySelector(".playerOneBtn")
-let PlayerTwoInputFiled = document.querySelector(".playerTwoInput")
-let playerTwoButton = document.querySelector(".playerTwoBtn")
-let displayedError = document.querySelector(".error")
-let heading = document.querySelector(".heading")
+playerOneButton.addEventListener('click', function () {
+  console.log(PlayerOneInputFiled.value);
 
-playerOneButton.addEventListener("click", function(){
-console.log(PlayerOneInputFiled.value)
+  if (PlayerOneInputFiled.value == '') {
+    displayedError.style.display = 'inline-block';
+  } else {
+    if (Number(PlayerOneInputFiled.value)) {
+      displayedError.innerHTML = '';
+      displayControl("playerOneBlockPlayerTwoNone");
 
-if(PlayerOneInputFiled.value == ""){
-  displayedError.style.display = "inline-block"
-  displayControl
-}
-else{
-  if(Number(PlayerOneInputFiled.value)){
-    displayedError.innerHTML = ""
-    displayControl()
-   
-     heading.innerHTML = "Player Two"
-     
+      heading.innerHTML = 'Player Two';
+    } else {
+      displayedError.style.display = 'inline-block';
+      displayedError.innerHTML = 'Please Give a Number LIKE(1,2,3)';
+    }
+    function displayControl(type) {
+      if (type == 'playerOneBlockPlayerTwoNone') {
+        PlayerTwoInputFiled.style.display = 'inline-block';
+        playerTwoButton.style.display = 'inline-block';
+        PlayerOneInputFiled.style.display = 'none';
+        playerOneButton.style.display = 'none';
+      } else if (type == 'playerOneNonePlayerTwoBlock') {
+        PlayerOneInputFiled.style.display = 'none';
+        playerOneButton.style.display = 'none';
+        PlayerTwoInputFiled.style.display = 'inline-block';
+        playerTwoButton.style.display = 'inline-block';
+      } else if (type == 'playerOneBlock') {
+        PlayerOneInputFiled.style.display = 'inline-block';
+        playerOneButton.style.display = 'inline-block';
+      } else if (type == playerOneNone) {
+        PlayerOneInputFiled.style.display = 'none';
+        playerOneButton.style.display = 'none';
+      }
+      else if (type == 'playerTwoBlock') {
+         PlayerTwoInputFiled.style.display = 'inline-block';
+        playerTwoButton.style.display = 'inline-block';
+      } else if (type == playerTwoNone) {
+        PlayerTwoInputFiled.style.display = 'none';
+        playerTwoButton.style.display = 'none';
+      }
+    }
   }
-  else{
-    displayedError.style.display = "inline-block"
-     displayedError.innerHTML = "Please Give a Number LIKE(1,2,3)"
-
-  }
- function displayControl (){
-   PlayerTwoInputFiled.style.display = "inline-block";
-     playerTwoButton.style.display = "inline-block"
-    PlayerOneInputFiled.style.display = "none";
-     playerOneButton.style.display = "none"
- }
-
-}
-})
+});
