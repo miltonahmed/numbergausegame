e93b207e3644ffa02b2eb74dc8584b9887f516b9
Dev Milton Ahemd <miltonahmed08@yahoo.com>
2025-05-25 01:39:04 +0400

    Function
    Make Function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 
-let PlayerOneInputFiled = document.querySelector("#playerOneInput")
+let PlayerOneInputFiled = document.querySelector(".playerOneInput")
 let playerOneButton = document.querySelector(".playerOneBtn")
+let PlayerTwoInputFiled = document.querySelector(".playerTwoInput")
+let playerTwoButton = document.querySelector(".playerTwoBtn")
 let displayedError = document.querySelector(".error")
+let heading = document.querySelector(".heading")
 
 playerOneButton.addEventListener("click", function(){
 console.log(PlayerOneInputFiled.value)
 
 if(PlayerOneInputFiled.value == ""){
   displayedError.style.display = "inline-block"
+  displayControl
 }
 else{
   if(Number(PlayerOneInputFiled.value)){
     displayedError.innerHTML = ""
-
-     
+    displayControl()
+   
+     heading.innerHTML = "Player Two"
      
   }
   else{
@@ -21,8 +26,12 @@ else{
      displayedError.innerHTML = "Please Give a Number LIKE(1,2,3)"
 
   }
- 
+ function displayControl (){
+   PlayerTwoInputFiled.style.display = "inline-block";
+     playerTwoButton.style.display = "inline-block"
+    PlayerOneInputFiled.style.display = "none";
+     playerOneButton.style.display = "none"
+ }
+
 }
-
-
 })
